c5389fe7e26ce664fa8bec3be420966bba454c38
bleistivt <bleistivt@users.noreply.github.com>
2016-05-24 22:34:28 +0200

    Add files via upload

---
+++ class.oneconversation.plugin.php
@@ -0,0 +1,98 @@
+<?php
+
+$PluginInfo['oneconversation'] = [
+    'Name' => 'One Conversation',
+    'Description' => 'Limit conversations to one per user.',
+    'Version' => '0.1',
+    'MobileFriendly' => true,
+    'SettingsUrl' => 'settings/oneconversation',
+    'SettingsPermission' => 'Garden.Settings.Manage',
+    'Author' => 'Bleistivt',
+    'AuthorUrl' => 'http://bleistivt.net',
+    'License' => 'GNU GPL2',
+    'GitHub' => 'bleistivt/oneconversation'
+];
+
+class OneConversationPlugin extends Gdn_Plugin {
+
+    public function messagesController_render_before($sender) {
+        if (!c('oneconversation.messageButton', true)) {
+            return;
+        }
+        // Check if the conversation editor has been opened with a single recipient.
+        if (inSection('PostConversation') && count($sender->RequestArgs) == 1) {
+            $to = Gdn::userModel()->getByUsername($sender->RequestArgs[0]);
+            // Is there already a conversation with this recipient?
+            if ($to && ($id = self::conversation(Gdn::session()->UserID, $to->UserID))) {
+                redirect('messages/'.$id.'#MessageForm');
+            }
+        }
+    }
+
+    public function messagesController_beforeAddConversation_handler($sender, $args) {
+        if (!c('oneconversation.autoAppend', true) || count($args['Recipients']) != 1) {
+            return;
+        }
+        if ($id = self::conversation(Gdn::session()->UserID, $args['Recipients'][0])) {
+            // Respond to an existing conversation instead of starting a new one.
+            $sender->ConversationMessageModel->save([
+                'ConversationID' => $id,
+                'Body' => $sender->Form->getFormValue('Body'),
+                'Format' => $sender->Form->getFormValue('Format')
+            ]);
+            redirect('messages/'.$id.'#MessageForm');
+        }
+    }
+
+
+    private static function conversation($from, $to) {
+        // Find all conversations that the sender has participated in.
+        $fromIDs = Gdn::sql()
+            ->select('uc.ConversationID')
+            ->from('UserConversation uc')
+            ->join('Conversation c', 'uc.ConversationID = c.ConversationID')
+            ->where('c.CountParticipants', 2)
+            ->where('uc.UserID', $from)
+            ->get()->resultArray();
+
+        // Find all conversations that the recipient has participated in.
+        $toIDs = Gdn::sql()
+            ->select('uc.ConversationID')
+            ->from('UserConversation uc')
+            ->join('Conversation c', 'uc.ConversationID = c.ConversationID')
+            ->where('c.CountParticipants', 2)
+            ->where('uc.UserID', $to)
+            ->get()->resultArray();
+
+        // Find all ConversationIDs where both users have participated.
+        $result = array_intersect(
+            array_column($fromIDs, 'ConversationID'),
+            array_column($toIDs, 'ConversationID')
+        );
+        return empty($result) ? false : max($result);
+    }
+
+
+    public function settingsController_oneConversation_create($sender) {
+        $sender->permission('Garden.Settings.Manage');
+        $sender->addSideMenu();
+
+        $conf = new ConfigurationModule($sender);
+        $conf->initialize([
+            'oneconversation.messageButton' => [
+                'Control' => 'checkbox',
+                'LabelCode' => 'Redirect "Message" button on profile pages to the most recent conversation.',
+                'Default' => true
+            ],
+            'oneconversation.autoAppend' => [
+                'Control' => 'checkbox',
+                'LabelCode' => 'Automatically add new conversation messages to the most recent conversation.',
+                'Default' => true
+            ]
+        ]);
+
+        $sender->title(sprintf(t('%s Settings'), 'One Conversation'));
+        $conf->renderAll();
+    }
+
+}
